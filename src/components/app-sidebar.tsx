e5fbe1b8dba504be344cc449fb54629255d70f98
yash-o <yashsky30@gmail.com>
2026-04-02 12:38:08 +0530

    10: payments

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -23,6 +23,7 @@ import {
     SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import { authClient } from "@/lib/auth-client";
+import { useHasActiveSubscription } from "@/features/subscriptions/hooks/use-subscription";
 
 const menuItems = [
     {
@@ -50,6 +51,7 @@ const menuItems = [
 export const AppSidebar = () => {
     const router = useRouter();
     const pathname = usePathname();
+    const { hasActiveSubscription, isLoading } = useHasActiveSubscription();
 
     return (
         <Sidebar collapsible="icon">
@@ -94,21 +96,23 @@ export const AppSidebar = () => {
             </SidebarContent>
             <SidebarFooter>
                 <SidebarMenu>
-                    <SidebarMenuItem>
-                        <SidebarMenuButton
-                            tooltip="Upgrade to Pro"
-                            className="gap-x-4 h-10 px-4"
-                            onClick={() => { }}
-                        >
-                            <StarIcon className="h-4 w-4" />
-                            <span> Upgrade to Pro</span>
-                        </SidebarMenuButton>
-                    </SidebarMenuItem>
+                    {!hasActiveSubscription && !isLoading && (
+                        <SidebarMenuItem>
+                            <SidebarMenuButton
+                                tooltip="Upgrade to Pro"
+                                className="gap-x-4 h-10 px-4"
+                                onClick={() => authClient.checkout({ slug: "pro" })}
+                            >
+                                <StarIcon className="h-4 w-4" />
+                                <span> Upgrade to Pro</span>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    )}
                     <SidebarMenuItem>
                         <SidebarMenuButton
                             tooltip="Billing Protal"
                             className="gap-x-4 h-10 px-4"
-                            onClick={() => { }}
+                            onClick={() => authClient.customer.portal()}
                         >
                             <CreditCardIcon className="h-4 w-4" />
                             <span>Billing Portal</span>
--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,15 +1,16 @@
 import { auth } from '@/lib/auth';
+import { polarClient } from '@/lib/polar';
 import { initTRPC, TRPCError } from '@trpc/server';
 import { headers } from 'next/headers';
 import { cache } from 'react';
- 
+
 export const createTRPCContext = cache(async () => {
   /**
    * @see: https://trpc.io/docs/server/context
    */
   return { userId: 'user_123' };
 });
- 
+
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
@@ -20,21 +21,38 @@ const t = initTRPC.create({
    */
   // transformer: superjson,
 });
- 
+
 // Base router and procedure helpers
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
-export const protectedProcedure = baseProcedure.use(async ({ ctx, next}) => {
+export const protectedProcedure = baseProcedure.use(async ({ ctx, next }) => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
 
-  if(!session) {
+  if (!session) {
     throw new TRPCError({
       code: 'UNAUTHORIZED',
       message: "Unauthorized",
     })
   }
-  return next({ ctx: { ...ctx, auth: session }}); 
+  return next({ ctx: { ...ctx, auth: session } });
 })
+export const premiumProcedure = protectedProcedure.use(async ({ ctx, next }) => {
+  const customer = await polarClient.customers.getStateExternal({
+    externalId: ctx.auth.user.id,
+  });
+
+  if (
+    !customer.activeSubscriptions ||
+    customer.activeSubscriptions.length === 0
+  ) {
+    throw new TRPCError({
+      code: 'FORBIDDEN',
+      message: "Active subscription required",
+    })
+  }
+  return next({ ctx: { ...ctx, customer } })
+},
+);

ff089379a913130f8f4449c281aff997e8a3cde2
yash-o <yashsky30@gmail.com>
2026-06-05 18:05:01 +0530

    12: workflows pagination

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,11 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { TRPCReactProvider } from "@/trpc/client";
 import { Toaster } from "sonner";
+import { NuqsAdapter } from 'nuqs/adapters/next/app'
+
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
+  variable: "--font-geist-sans", 
   subsets: ["latin"],
 });
 
@@ -30,8 +32,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <TRPCReactProvider>
+        <NuqsAdapter>
           {children}
           <Toaster />
+        </NuqsAdapter>
         </TRPCReactProvider>
       </body>
     </html>
--- src/features/workflows/hooks/use-workflows.ts
+++ src/features/workflows/hooks/use-workflows.ts
@@ -2,11 +2,13 @@ import { useTRPC } from "@/trpc/client"
 import { useMutation, useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
+import { useWorkflowsParams } from "./use-workflows-params";
 
 export const useSuspenseWorkflows = () => {
-    const trpc = useTRPC();
+    const trpc = useTRPC(); 
+    const [params] = useWorkflowsParams();
 
-    return useSuspenseQuery(trpc.workflows.getMany.queryOptions());
+    return useSuspenseQuery(trpc.workflows.getMany.queryOptions(params));
 }
 
 export const useCreateWorkflow = () => {
@@ -18,7 +20,7 @@ export const useCreateWorkflow = () => {
             onSuccess: (data) => {
                 toast.success(`Workflow "${data.name}" created`);
                 queryClient.invalidateQueries(
-                    trpc.workflows.getMany.queryOptions(),
+                    trpc.workflows.getMany.queryOptions({}),
                 );
             },
             onError: (error) => {
